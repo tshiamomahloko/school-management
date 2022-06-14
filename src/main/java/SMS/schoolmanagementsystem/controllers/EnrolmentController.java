a784d1a66e8701fffafa9f096afb768a467f23d9
KiyolinP <kiyolinp@bbd.co.za>
2022-06-14 13:12:59 +0200

    removed commented out code

--- src/main/java/SMS/schoolmanagementsystem/controllers/EnrolmentController.java
+++ src/main/java/SMS/schoolmanagementsystem/controllers/EnrolmentController.java
@@ -1,23 +1,18 @@
 package SMS.schoolmanagementsystem.controllers;
 
 import SMS.schoolmanagementsystem.models.Enrolment;
-import SMS.schoolmanagementsystem.repositories.EnrolmentsRepository;
 import SMS.schoolmanagementsystem.services.EnrolmentsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.rmi.ServerException;
 import java.util.List;
 
 @RestController
 public class EnrolmentController {
-    //private final EnrolmentsRepository enrolmentsRepository;
 
     @Autowired
     private EnrolmentsService enrolmentsService;
@@ -33,27 +28,6 @@ public class EnrolmentController {
         return enrolmentsService.getEnrolments();
     }
 
-//    public EnrolmentController(EnrolmentsRepository enrolmentsRepository) {
-//        this.enrolmentsRepository = enrolmentsRepository;
-//    }
 
-//    @GetMapping("/enrolments")
-//    List<Enrolment> allEnrolments() {
-//        return enrolmentsRepository.getEnrolments();
-//    }
-//
-//    @PostMapping("/enrolments")
-//    public Enrolment createEnrolment(@RequestBody Enrolment newEnrolment) {
-//        Enrolment enrolment = enrolmentsRepository.save(newEnrolment);
-//        if (enrolment == null) {
-//            try {
-//                throw new ServerException("creating enrolment failed");
-//            } catch (ServerException e) {
-//                throw new RuntimeException(e);
-//            }
-//        } else {
-//            return enrolment;
-//        }
-//    }
 
 }
