b23d3e9199f71a4d9ab21f95efc570ad67e61eb8
yaqwaqwa <103098964+yaqwaqwa@users.noreply.github.com>
2022-06-14 20:00:53 +0200

    User journey updated

--- src/main/java/SMS/schoolmanagementsystem/controllers/UserController.java
+++ src/main/java/SMS/schoolmanagementsystem/controllers/UserController.java
@@ -7,26 +7,53 @@ import SMS.schoolmanagementsystem.repositories.UserTypeRepository;
 import SMS.schoolmanagementsystem.repositories.UsersRepository;
 import SMS.schoolmanagementsystem.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
-@RestController @RequiredArgsConstructor @RequestMapping(path = "users")
+@RestController @RequiredArgsConstructor @RequestMapping(path = "users") @Slf4j
 public class UserController {
 
     @Autowired
     private UserService service;
 
     @GetMapping("{userId}/get-user-info")
-    public Users getUser(@PathVariable("userId") int id) {
-        return service.getUser(id);
+    public ResponseEntity<?> getUser(@PathVariable("userId") int id) {
+        try {
+            Users user = service.getUser(id);
+            return ResponseEntity.ok(user);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
-    @GetMapping("")
-    public List<Users> getAllUsers() {
-        return service.getAllUsers();
+    @GetMapping("get-all-users")
+    public ResponseEntity<?> getAllUsers(Principal principal) {
+        log.info(String.valueOf(principal));
+        return ResponseEntity.ok(service.getAllUsers());
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<?> addUser(@RequestBody Users newUser) {
+        try {
+            Users user = service.addUser(newUser);
+            return ResponseEntity.accepted().body(user);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("{userId}/edit-details")
+    public ResponseEntity<?> editUser(@PathVariable("userId") int id, @RequestBody Users updateUser) {
+        try {
+            return ResponseEntity.accepted().body(service.editUser(id, updateUser));
+        } catch (IllegalStateException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
